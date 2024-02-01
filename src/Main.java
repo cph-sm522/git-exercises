b7d5eb616fec4e44b579c769b27ced4a146451ac
cph-sm522 <cph-sm522@cphbusiness.dk>
2024-02-01 10:14:44 +0100

    hej

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; 1 <= 100; i++) {
+            System.out.println(i);
             if (100 % 3 == 0) {
                 System.out.println("Fizz");
             } else if (100 % 5 == 0) {
