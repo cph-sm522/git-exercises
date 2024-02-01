91b2be6230d14040259cb5341c98a92222887d57
Patrick Nyboe Kjøller <Patrick@ctrl.dk>
2024-02-01 10:26:02 +0100

    hh

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         System.out.printf("FizzBuzz Tæl op:");
 
         for (int i = 1; i <= 100; i++) {
-            if(100 % 3 == 0){
-                System.out.println("Fizz");
-            } else if(100 % 5 == 0){
-                System.out.println("Buzz");
-            } else if (100 % 3 == 0 && 100 % 5 == 0){
-                System.out.println("FizzBuzz");
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println(i + " FizzBuzz");
+            } else if(i % 5 == 0){
+                System.out.println(i + " Buzz");
+            } else if (i % 3 == 0){
+                System.out.println(i + " Fizz");
             } else {
                 System.out.println(i);
             }
