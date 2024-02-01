cece0e465516ada34514309736825e5cd23251ab
cph-sm522 <cph-sm522@cphbusiness.dk>
2024-02-01 09:56:28 +0100

    hej

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+
+    public void numbers(){
+
+        for(int i = 1; 1 <= 100; i++) {
+            if (100 % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (100 % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (100 % 3 == 0 && 100 % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+        }
+    }
 }
